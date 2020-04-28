1be5d16271b93ffdf48f0f0574a5cf0432f2f0bd
Garro97 <64464777+Garro97@users.noreply.github.com>
2020-04-28 20:42:37 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -24,6 +24,8 @@ def make_dataset(directory, class_to_idx, filename):
     # READ FILE AND IMAGES
     with open(input_file, "r") as file:
         for line in file:
+            if line.endswith('\n'):
+                line = line.rstrip()
             class_name = line.split("/")[0]
             if (not class_name.startswith("BACKGROUND")):
                 class_index = class_to_idx[class_name]
